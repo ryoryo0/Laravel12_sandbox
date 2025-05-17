19604d38d243808d0efd8f5eb150e7bd61f2f7f4
ryosukeharada <web.harada1111@gmail.com>
2025-05-17 22:27:01 +0900

    userとroleのseederファイルの追加

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // 管理者を作成
+
+        // スタッフを作成
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // 管理者を作成
+
+        // スタッフを作成
+
+        // 一般訪問者を作成
+    }
+}
