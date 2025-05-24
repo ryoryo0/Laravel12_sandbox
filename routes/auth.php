2384d9a3d140f3cac747d7615505d11919c42e7a
ryosukeharada <web.harada1111@gmail.com>
2025-05-24 10:26:06 +0900

    customer用のroute設置と controllerファイルを実装

--- routes/auth.php
+++ routes/auth.php
@@ -26,37 +26,24 @@ Route::prefix('admin')->name('admin.')->middleware('guest:admin')->group(functio
 });
 
 Route::prefix('admin')->middleware('auth:admin')->group(function () {
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    // Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    // Route::get('/dashboard', function () {
+    //     return view('dashboard');
+    // })->name('dashboard');
 });
 
 /**
  * Customer
  */
 Route::prefix('customer')->middleware('guest:customer')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
-
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
-
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-        ->name('password.request');
-
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->name('password.email');
-
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name('password.reset');
-
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
+    Route::get('login', [App\Http\Controllers\Customer\Auth\AuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('login', [App\Http\Controllers\Customer\Auth\AuthenticatedSessionController::class, 'store']);
+    Route::get('register', [App\Http\Controllers\Customer\Auth\RegisteredUserController::class, 'create'])->name('register');
+    Route::post('register', [App\Http\Controllers\Customer\Auth\RegisteredUserController::class, 'store']);
+    Route::get('forgot-password', [App\Http\Controllers\Customer\Auth\PasswordResetLinkController::class, 'create'])->name('password.request');
+    Route::post('forgot-password', [App\Http\Controllers\Customer\Auth\PasswordResetLinkController::class, 'store'])->name('password.email');
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create']) ->name('password.reset');
+    Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
 });
 
 Route::prefix('customer')->middleware('auth:customer')->group(function () {
