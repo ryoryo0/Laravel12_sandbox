6808c0f0978beb1a01065e86add0908bc4dd8929
ryosukeharada <web.harada1111@gmail.com>
2025-05-31 10:07:54 +0900

    authのコンフィグファイルの設定に記述を追加

--- config/auth.php
+++ config/auth.php
@@ -113,6 +113,18 @@ return [
             'expire' => 60,
             'throttle' => 60,
         ],
+        'admins' => [
+            'provider' => 'admins',
+            'table' => 'admin_password_reset_tokens',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+        'customers' => [
+            'provider' => 'customers',
+            'table' => 'customer_password_reset_tokens',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
     ],
 
     /*
