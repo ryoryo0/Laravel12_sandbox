3d4ee6b75922a78d03c9e6635820e1156fbfdc46
ryosukeharada <web.harada1111@gmail.com>
2025-05-31 10:10:50 +0900

    未認証時のリダイレクト処理の追加

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Http\Middleware\RedirectIfCustomerAuthenticated;
-use App\Http\Middleware\RedirectIfCustomerNotAuthenticated;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -15,14 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        
     )
 
     ->withMiddleware(function (Middleware $middleware) {
-        // 非ログインユーザー用のリダイレクト設定
-        $middleware->alias([
-            'guest.customer' => RedirectIfCustomerAuthenticated::class,
-            'auth.customer' => RedirectIfCustomerNotAuthenticated::class,
-        ]);
+        $middleware->redirectGuestsTo(function ($request) {
+            return $request->is('admin*') ? route('admin.login') : route('customer.login');
+        });
     })
 
     ->withExceptions(function (Exceptions $exceptions) {
