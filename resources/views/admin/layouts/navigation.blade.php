a140f3091dcb2bdded60859b91055d6fc2b10ab3
ryosukeharada <web.harada1111@gmail.com>
2025-05-31 10:55:57 +0900

    ログインブレードファイルの軽微な調整

--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -75,8 +75,8 @@
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::guard('customer')->user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::guard('customer')->user()->email }}</div>
+                <div class="font-medium text-base text-gray-800">{{ Auth::guard('admin')->user()->name }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ Auth::guard('admin')->user()->email }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
@@ -85,10 +85,10 @@
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
-                <form method="POST" action="{{ route('customer.logout') }}">
+                <form method="POST" action="{{ route('admin.logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('customer.logout')"
+                    <x-responsive-nav-link :href="route('admin.logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
