ecff5b266e7faead9224ed63b119d4f51e245f48
ryosukeharada <web.harada1111@gmail.com>
2025-06-01 15:03:12 +0900

    modify admin用のrequestファイル追加と調整

--- app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\admin\Auth;
 
 use App\Http\Controllers\Admin\Controller;
-use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Admin\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
