76ce37c9349774262c2eb19d9458c91523c1116b
ryosukeharada <web.harada1111@gmail.com>
2025-06-01 14:46:32 +0900

    modify  customerのprofile controllerの戻り値を修正

--- app/Http/Controllers/Customer/ProfileController.php
+++ app/Http/Controllers/Customer/ProfileController.php
@@ -17,7 +17,7 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
-        return view('profile.edit', [
+        return view('customer.profile.edit', [
             'user' => $request->user(),
         ]);
     }
@@ -35,7 +35,7 @@ class ProfileController extends Controller
 
         $request->user()->save();
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return Redirect::route('customer.profile.edit')->with('status', 'profile-updated');
     }
 
     /**
