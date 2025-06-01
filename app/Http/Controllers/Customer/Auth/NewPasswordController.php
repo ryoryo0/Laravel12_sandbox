d6b1bbfa425a6e170dc87424c266add7fd815542
ryosukeharada <web.harada1111@gmail.com>
2025-06-01 15:19:49 +0900

    modify customer用のログイン前のパスワード変更通知の作成と調整

--- app/Http/Controllers/Customer/Auth/NewPasswordController.php
+++ app/Http/Controllers/Customer/Auth/NewPasswordController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Customer\Auth;
 
 use App\Http\Controllers\Customer\Controller;
-use App\Models\User;
+use App\Models\Customer;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -36,22 +36,22 @@ class NewPasswordController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
+        // Here we will attempt to reset the customer's password. If it is successful we
+        // will update the password on an actual customer model and persist it to the
         // database. Otherwise we will parse the error and return the response.
-        $status = Password::reset(
+        $status = Password::broker('customers')->reset(
             $request->only('email', 'password', 'password_confirmation', 'token'),
-            function (User $user) use ($request) {
-                $user->forceFill([
+            function (Customer $customer) use ($request) {
+                $customer->forceFill([
                     'password' => Hash::make($request->password),
                     'remember_token' => Str::random(60),
                 ])->save();
-
-                event(new PasswordReset($user));
+        
+                event(new PasswordReset($customer));
             }
         );
 
-        // If the password was successfully reset, we will redirect the user back to
+        // If the password was successfully reset, we will redirect the customer back to
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
         return $status == Password::PASSWORD_RESET
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
+use App\Notifications\CustomerResetPasswordNotification;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Contracts\Auth\CanResetPassword;
+use Illuminate\Auth\Passwords\CanResetPassword as CanResetPasswordTrait;
+use Illuminate\Notifications\Notifiable;
 
 class Customer extends Authenticatable
 {
     use SoftDeletes;
+    use CanResetPasswordTrait;
+    use Notifiable;
 
     protected $guard_name = 'customer';
 
@@ -33,5 +39,10 @@ class Customer extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new CustomerResetPasswordNotification($token));
     }
 }
