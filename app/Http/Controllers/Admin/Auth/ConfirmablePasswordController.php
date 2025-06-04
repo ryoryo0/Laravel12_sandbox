f5ddd1598ade4fb195f9e3e22ede4e9616ea9ce5
ryosukeharada <web.harada1111@gmail.com>
2025-06-04 18:31:49 +0900

    軽微な修正

--- app/Http/Controllers/Admin/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Admin/Auth/ConfirmablePasswordController.php
@@ -33,7 +33,7 @@ class ConfirmablePasswordController extends Controller
             ]);
         }
 
-        $request->session()->put('auth.password_confirmed_at', time());
+        $request->session(['guard' => 'admin'])->put('auth.password_confirmed_at', time());
 
         return redirect()->intended(route('dashboard', absolute: false));
     }
