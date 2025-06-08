66cebb0ecbbb8051e3d50c0841166042e516e980
ryosukeharada <web.harada1111@gmail.com>
2025-06-08 18:21:34 +0900

    add products用のmigrationファイルを追加

--- database/migrations/2025_06_07_210235_create_products_table.php
+++ database/migrations/2025_06_07_210235_create_products_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('sub_name');
+            $table->integer('category_id');
+            $table->ulid('ulid');
+            $table->boolean('is_public');
+            $table->boolean('is_pick_up');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
