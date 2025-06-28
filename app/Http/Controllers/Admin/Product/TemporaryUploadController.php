c904b6e269ebaa47d034c48b24715e60a1ef7e75
ryosukeharada <web.harada1111@gmail.com>
2025-06-28 19:01:36 +0900

    一時ファイルのストレージへの保存処理を作成

--- app/Http/Controllers/Admin/Product/TemporaryUploadController.php
+++ app/Http/Controllers/Admin/Product/TemporaryUploadController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers\Admin\Product;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class TemporaryUploadController
 {
     public function __invoke(Request $request)
     {
-        $path = $request->file('image')->store('temp');
+        $image = new \Imagick($request->file('image')->getRealPath());
+        $image->thumbnailImage(200, 150, true);
+        $imageData = $image->getImageBlob();
+        $dir = 'temporary';
+        $filename = 'thumbnail_' . Str::random(16) . '.jpg';
+        Storage::disk('public')->put( $dir . '/' . $filename, $imageData);
+        $url = asset('storage/' . $dir . '/' . $filename);
         return response()->json([
-            'url' => asset('storage/' . $path),
-            'path' => $path
+            'url' => $url,
+            'path' => $dir . '/' . $filename
         ]);
     }
 }
