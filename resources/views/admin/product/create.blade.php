d32ce1941299854f7e66c0dedd8f3a35a98ff48b
ryosukeharada <web.harada1111@gmail.com>
2025-07-01 19:23:08 +0900

    軽微な調整

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -33,7 +33,7 @@
       </div>
       <div class="mb-4">
         <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900">商品コード</label>
-        <input type="text" name="code" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <input type="text" name="code" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500">
       </div>
       <div class="mb-4">
           <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-900" for="file_input">サムネイル画像</label>
