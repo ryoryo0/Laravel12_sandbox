6f1f6f9eb5bdc119fc021512831fd37b6f15cf57
ryosukeharada <web.harada1111@gmail.com>
2025-07-13 11:02:15 +0900

    登録処理に必要なファイルの作成

---
+++ app/Http/Controllers/Admin/Product/StoreController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Product;
+
+use Illuminate\Http\Request;
+
+class StoreController
+{
+    public function __invoke(Request $request)
+    {
+
+    }   
+}
--- routes/admin.php
+++ routes/admin.php
@@ -36,6 +36,7 @@ Route::prefix('admin')->name('admin.')->middleware(['auth:admin'])->group(functi
     Route::prefix('/product')->name('product.')->group(function () {
         Route::get('/', App\Http\Controllers\Admin\Product\IndexController::class)->name('index');
         Route::get('/create', App\Http\Controllers\Admin\Product\CreateController::class)->name('create');
+        Route::post('/store', App\Http\Controllers\Admin\Product\StoreController::class)->name('store');
         Route::post('/upload-temp', App\Http\Controllers\Admin\Product\TemporaryUploadController::class)->name('upload-temp');
     });
 });
