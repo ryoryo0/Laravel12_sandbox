94177daf4b6f30e0a73093c549a862fbdfd0e984
ryosukeharada <web.harada1111@gmail.com>
2025-08-03 11:42:47 +0900

    商品の登録処理を作成

--- app/Http/Controllers/Admin/Product/StoreController.php
+++ app/Http/Controllers/Admin/Product/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Product;
 
 use App\Http\Requests\Admin\Product\StoreRequest;
 use App\Models\Product;
+use App\Models\TemporaryImage;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -15,14 +16,36 @@ class StoreController
     public function __invoke(StoreRequest $request)
     {
         try {
-            $product = new Product();
             $validated = $request->validated();
             $validated['create_admin_id'] = Auth::user()->id;
             $validated['ulid'] = Str::ulid();
-            DB::transaction(function () use ($product, $validated) {
-                $product->fill($validated)->save();
+
+            $thumbImage = TemporaryImage::query()
+                ->where('ulid', $validated['thumbnail'])
+                ->first()
+                ->toArray();
+            $thumbImage['is_thumbnail'] = true;
+
+            $otherImages = TemporaryImage::query()
+                ->whereIn('ulid', $validated['other_thumbnail'])
+                ->get()
+                ->toArray();
+        
+            foreach ($otherImages as $image) {
+                $image['is_thumbnail'] = false;
+            }
+
+            DB::transaction(function () use ($validated, $thumbImage, $otherImages) {
+                // 商品登録
+                $product = Product::create($validated);
+                // 商品カテゴリー登録
+                $product->categories()->sync($validated['category_ids']);
+                // 商品の画像登録
+                $product->images()->create($thumbImage);
+                $product->images()->createMany($otherImages);
+                // 登録　完了のログ
+                Log::info('product create', ['product_id' => $product->id]);
             });
-            Log::info('product create', ['product_id' => $product->id]);
             return redirect()->route('admin.product.index')->with('success', '商品の登録が完了しました');
         } catch (Throwable $e)  {
             Log::error($e);
--- app/Http/Requests/Admin/Product/StoreRequest.php
+++ app/Http/Requests/Admin/Product/StoreRequest.php
@@ -23,14 +23,20 @@ class StoreRequest extends FormRequest
     public function rules(): array
     {
         $rules = [
-            'name'        => ['nullable','string', 'max:255'],
-            'description' => ['nullable','string', 'max:255'],
+            'name'         => ['nullable','string', 'max:255'],
+            'description'  => ['nullable','string', 'max:255'],
             'category_ids' => ['nullable','array', Rule::exists('categories', 'id')],
-            'code'        => ['nullable','string', 'max:255', Rule::unique('products', 'code')],
-            'detail'      => ['nullable','string', 'max:255'],
-            'is_public'   => ['nullable','boolean'],
-            'is_pick_up'  => ['nullable','boolean'],
+            'code'         => ['nullable','string', 'max:255', Rule::unique('products', 'code')],
+            'detail'       => ['nullable','string', 'max:255'],
+            'is_public'    => ['nullable','boolean'],
+            'is_pick_up'   => ['nullable','boolean'],
         ];
+
+        $rules += [
+            'thumbnail'       => ['nullable', 'string'],
+            'other_thumbnail' => ['nullable', 'array'],
+        ];
+
         return $rules;
     }
 }
