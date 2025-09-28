8906f78079144a9128a85d3a4b56bc8bc1d7409f
ryosukeharada <web.harada1111@gmail.com>
2025-09-28 12:50:15 +0900

    余分なinputを削除

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -144,7 +144,6 @@
           <div>
           <label class="block mb-3 text-sm font-medium text-gray-900 dark:text-white">その他の画像 <span class="text-sm text-gray-500">(最大4枚)</span></label>
           <div class="border-2 border-dashed border-gray-300 dark:border-gray-600 rounded-lg p-6 text-center hover:border-gray-400 dark:hover:border-gray-500 transition-colors">
-            <input type="file" id="mulch-file_input" data-js="upload-multiple-temporary-input" accept="image/*" multiple class="hidden">
             <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48">
               <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 1 5.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
