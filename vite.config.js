cb06837b2a4b8ad9aca64d6642e19a8f66ecfaba
ryosukeharada <web.harada1111@gmail.com>
2025-10-04 07:12:06 +0900

    npm runでコマンドが走らなくなったのでvite.configファイルの内容を修正実施

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite'
-import fs from 'fs';
+    import { defineConfig } from 'vite';
+    import laravel from 'laravel-vite-plugin';
+    import tailwindcss from '@tailwindcss/vite'
+    import fs from 'fs';
+    import glob from 'fast-glob'; 
 
-export default defineConfig({
-    server: {
-        https: {
-            key: fs.readFileSync('/etc/ssl/certs/laravel12.local-key.pem'),
-            cert: fs.readFileSync('/etc/ssl/certs/laravel12.local.pem'),
+    export default defineConfig({
+        server: {
+            https: {
+                key: fs.readFileSync('/etc/ssl/certs/laravel12.local-key.pem'),
+                cert: fs.readFileSync('/etc/ssl/certs/laravel12.local.pem'),
+            },
+            host: '0.0.0.0',
+            port: 5173,
         },
-        host: '0.0.0.0',
-        port: 5173,
-    },
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                ...glob.sync('resources/js/pages/**/*.js'),
-                ...glob.sync('resources/js/libraries/**/*.js'),
-            ],
-            refresh: true,
-            hotFile: 'public/hot',
-        }),tailwindcss(),
-    ],
-});
+        plugins: [
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                    ...glob.sync('resources/js/pages/**/*.js'),
+                    ...glob.sync('resources/js/libraries/**/*.js'),
+                ],
+                refresh: true,
+                hotFile: 'public/hot',
+            }),tailwindcss(),
+        ],
+    });
