5c5df805d0fed04ca4e5b41a5eb6e5b6e3d415aa
ryosukeharada <web.harada1111@gmail.com>
2025-10-05 09:03:51 +0900

    modelファイルを追加

---
+++ app/Models/ProductVariant.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Validation\Rule;
+
+class ProductVariant extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'product_id',
+        'color',
+        'size',
+        'stock',
+        'price',
+    ];
+
+    /**
+     * ---------------------------------------------------------------------------------------------------------------------------------------------
+     * Relation
+     * ---------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * ---------------------------------------------------------------------------------------------------------------------------------------------
+     * Method
+     * ---------------------------------------------------------------------------------------------------------------------------------------------
+     */
+
+    /**
+     * 基本のバリデーションルールを取得
+     *
+     * @return array
+     */
+    public static function getBaseRules(): array
+    {
+        $rules = [
+            'product_id' => ['required', 'integer', Rule::exists('products', 'id')],
+            'color'      => ['required', 'string', 'max:255'],
+            'size'       => ['required', 'string', 'max:255'],
+            'stock'      => ['required', 'integer', 'min:0'],
+            'price'      => ['required', 'numeric', 'min:0'],
+        ];
+
+        return $rules;
+    }
+}
