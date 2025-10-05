f96cbc3f3dc769b605a8dcc5bc69fe2c49c13e28
ryosukeharada <web.harada1111@gmail.com>
2025-10-05 12:11:18 +0900

    コードのリファクタリング

--- app/Http/Controllers/Admin/Actions/Event/CreateAction.php
+++ app/Http/Controllers/Admin/Actions/Event/CreateAction.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Actions\Event;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class CreateAction
@@ -11,7 +12,17 @@ class CreateAction
     public function execute(Request $request): View
     {
         $adminUser = Auth::user();
-        $products = $adminUser->products()->pluck('name', 'id');
+
+        // すでにイベントが付与されている商品IDを取得
+        $productsWithEvents = DB::table('event_product')
+            ->pluck('product_id')
+            ->unique()
+            ->toArray();
+
+        // 管理者の商品から、すでにイベントが付与されている商品を除外
+        $products = $adminUser->products()
+            ->whereNotIn('products.id', $productsWithEvents)
+            ->pluck('name', 'id');
 
         return view('admin.event.create')
             ->with([
--- app/Http/Controllers/Admin/Actions/Event/EditAction.php
+++ app/Http/Controllers/Admin/Actions/Event/EditAction.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\Actions\Event;
 use App\Models\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class EditAction
@@ -20,7 +21,25 @@ class EditAction
 
         // 関連データをロード
         $event->load(['products', 'image']);
-        $products = $adminUser->products()->pluck('name', 'id');
+
+        // 他のイベント（現在編集中のイベント以外）に紐づいている商品IDを取得
+        $productsWithOtherEvents = DB::table('event_product')
+            ->where('event_id', '!=', $event->id)
+            ->pluck('product_id')
+            ->unique()
+            ->toArray();
+
+        // 現在のイベントに紐づいている商品ID
+        $currentEventProductIds = $event->products->pluck('id')->toArray();
+
+        // 除外すべき商品ID = 他のイベントに紐づいている商品ID - 現在のイベントに紐づいている商品ID
+        $excludeProductIds = array_diff($productsWithOtherEvents, $currentEventProductIds);
+
+        // 管理者の商品から、他のイベントに紐づいている商品を除外
+        $products = $adminUser->products()
+            ->whereNotIn('products.id', $excludeProductIds)
+            ->pluck('name', 'id');
+
         return view('admin.event.edit')
             ->with([
                 'products' => $products,
